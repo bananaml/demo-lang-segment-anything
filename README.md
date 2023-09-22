31d7a0bff6f0ee04737f36ad3f35134e96be40e0
yachty66 <maxhager28@gmail.com>
2023-09-22 16:11:52 +0200

    testing purposes

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# demo-lang-segment-anything
+![](https://www.banana.dev/lib_zOkYpJoyYVcAamDf/x2p804nk9qvjb1vg.svg?w=340 "Banana.dev")
+
+# Banana.dev lang-segment-anything starter template
+
+This is a lang-segment-anything starter template from [Banana.dev](https://www.banana.dev) that allows on-demand serverless GPU inference.
+
+You can fork this repository and deploy it on Banana as is, or customize it based on your own needs.
+
+# Running this app
+
+## Deploying on Banana.dev
+
+1. [Fork this](https://github.com/bananaml/demo-lang-segment-anything/fork) repository to your own Github account.
+2. Connect your Github account on Banana.
+3. [Create a new model](https://app.banana.dev/deploy) on Banana from the forked Github repository.
+
+## Running after deploying
+
+1. Wait for the model to build after creating it.
+2. Make an API request using one of the provided snippets in your Banana dashboard. However, instead of sending a prompt as provided in the snippet, send structure your request as following:
+
+```python
+inputs = {
+    "image": "image_string_base64",
+    "box_threshold": "your_box_threshold",
+    "text_threshold": "your_text_threshold",
+    "promt": "your_text_prompt"
+
+}
+```
+
+For more info, check out the [Banana.dev docs](https://docs.banana.dev/banana-docs/).
--- app.py
+++ app.py
@@ -9,8 +9,18 @@ from lang_sam.utils import load_image
 import base64
 from PIL import Image
 import io
+import os
+from dotenv import load_dotenv
+import boto3
+
+load_dotenv()
 
 app = Potassium("lang-segment-anything")
+
+AWS_ID = os.getenv("AWS_ID")
+AWS_KEY = os.getenv("AWS_KEY")
+AWS_REGION = os.getenv("AWS_REGION")
+AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
 
 @app.init
 def init():
@@ -27,7 +37,41 @@ def handler(context: dict, request: Request) -> Response:
     text_threshold = request.json.get("text_threshold")
     text_prompt = request.json.get("prompt")
     model = context.get("model")
-    image_data = base64.b64decode(image_base64)
+
+
+
+    image_pil = Image.open("banana.png").convert("RGB")
+    image_array = np.asarray(image_pil)
+    masks, boxes, phrases, logits = model.predict(image_pil, text_prompt, box_threshold, text_threshold)
+    labels = [f"{phrase} {logit:.2f}" for phrase, logit in zip(phrases, logits)]
+    image = draw_image(image_array, masks, boxes, labels)
+    image = Image.fromarray(np.uint8(image)).convert("RGB")
+    image.save('output.png')
+
+
+
+    #upload to s3
+    session = boto3.Session(
+        aws_access_key_id=AWS_ID,
+        aws_secret_access_key=AWS_KEY,
+        region_name=AWS_REGION
+    )
+    s3 = session.client('s3')
+    bucket_name = AWS_BUCKET_NAME
+    with open("output.png", 'rb') as data:
+        s3.upload_fileobj(data, bucket_name, "output.png")
+    url = f"https://{bucket_name}.s3.amazonaws.com/djasjdmaklmflln"
+
+
+
+    return Response(json={"output": url}, status=200)
+
+
+
+
+
+
+    """image_data = base64.b64decode(image_base64)
     image_pil = Image.open(io.BytesIO(image_data)).convert("RGB")
     masks, boxes, phrases, logits = model.predict(image_pil, text_prompt, box_threshold, text_threshold)
     labels = [f"{phrase} {logit:.2f}" for phrase, logit in zip(phrases, logits)]
@@ -37,7 +81,7 @@ def handler(context: dict, request: Request) -> Response:
     buffered = io.BytesIO()
     image.save(buffered, format="JPEG")
     img_str = base64.b64encode(buffered.getvalue()).decode()
-    return Response(json={"output": img_str}, status=200)
+    return Response(json={"output": img_str}, status=200)"""
     
 if __name__ == "__main__":
     app.serve()
