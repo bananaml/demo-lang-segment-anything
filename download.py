e77e472e228888c860266e7a568f477acf4d3470
yachty66 <maxhager28@gmail.com>
2023-09-23 17:12:10 +0200

    remove download.py

--- download.py
+++ download.py
@@ -1,29 +1,9 @@
-from lang_sam import LangSAM
-import torch
-import logging
+#from lang_sam import LangSAM
+
 
 def download_model():
-    model = LangSAM()
-
-    # Print PyTorch and CUDA versions
-    print("PyTorch version:", torch.__version__)
-    print("CUDA version:", torch.version.cuda)
-     # Log PyTorch and CUDA versions
-    logging.info("PyTorch version: %s", torch.__version__)
-    logging.info("CUDA version: %s", torch.version.cuda)
-
-    # Check GPU availability
-    logging.info("Number of GPUs available: %d", torch.cuda.device_count())
-    if torch.cuda.is_available():
-        logging.info("GPU device name: %s", torch.cuda.get_device_name(0))
-    else:
-        logging.info("No GPU available.")
-    # Check GPU availability
-    print("Number of GPUs available:", torch.cuda.device_count())
-    if torch.cuda.is_available():
-        print("GPU device name:", torch.cuda.get_device_name(0))
-    else:
-        print("No GPU available.")
+    #model = LangSAM()
+    pass
 
 if __name__ == "__main__":
     download_model()
